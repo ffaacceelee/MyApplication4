4d13374d4bbde4fa068d53105d8adbccf202f74d
liyan <face_l@163.com>
2018-01-28 01:20:38 +0800

    android github

---
+++ app/src/main/java/com/example/liyan/myapplication/Main2Activity.java
@@ -0,0 +1,36 @@
+package com.example.liyan.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView tv = (TextView)findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String t = intent.getStringExtra("key");
+        tv.setText(t);
+    }
+
+}
